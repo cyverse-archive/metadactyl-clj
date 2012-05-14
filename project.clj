f18415150ce39eb1ea2a858994dd54529bcf5769
Dennis Roberts <dennis@iplantcollaborative.org>
2012-05-14 10:41:56 -0700

    replaced reference_genomes.json

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
   :main metadactyl.core
   :ring {:handler metadactyl.core/app :init metadactyl.core/load-configuration}
   :iplant-rpm {:summary "iPlant Discovery Environment Metadata Services"
-               :release 1
+               :release 2j
                :provides "metadactyl"
                :dependencies ["iplant-service-config >= 0.1.0-4"]
                :config-files ["log4j.properties" "reference_genomes.json"]
