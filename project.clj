efe5aa3ffc99ab252a9929a8562cbe9b5fb82bf7
Dennis Roberts <dennis@iplantcollaborative.org>
2013-09-12 10:37:23 -0700

    bumped the version number

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [compojure "1.1.5"]
                  [org.iplantc/clojure-commons "1.4.1-SNAPSHOT"]
                  [org.iplantc/kameleon "0.1.3-SNAPSHOT"]
-                 [org.iplantc.core/metadactyl "1.8-SNAPSHOT"]
+                 [org.iplantc.core/metadactyl "1.8.2-SNAPSHOT"]
                  [org.springframework/spring-orm "3.1.0.RELEASE"]
                  [korma "0.3.0-RC5"]
                  [ring "1.1.8"]
